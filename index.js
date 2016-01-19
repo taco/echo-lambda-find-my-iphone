1e817c748af4b060cea00bc3d175ccd978a0f3b8
Travis Johnson <travis_johnson@m02995.corp.volusion.com>
2016-01-19 16:35:33 -0600

    Made base64 encoded password

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var find = require('find-my-iphone');
 exports.handler = function(event, context) {
     console.log('Attempting to find ', event.phone);
 
-    find(process.env.ICLOUD_USERNAME, process.env.ICLOUD_PASSWORD, event.phone, function () {
+    find(process.env.ICLOUD_USERNAME, new Buffer(process.env.ICLOUD_PASSWORD, 'base64').toString(), event.phone, function () {
         console.log('Successfully found ', event.phone);
     });
 };
