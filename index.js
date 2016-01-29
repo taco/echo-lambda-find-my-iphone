2f83c4bfe2180711c2922ff4fd368c154c856eeb
Taco <travis_johnson@volusion.com>
2016-01-28 19:47:21 -0600

    Added some error handling with logging

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var find = require('find-my-iphone');
 exports.handler = function(event, context) {
     console.log('Attempting to find ', event.phone);
 
-    find(process.env.ICLOUD_USERNAME, new Buffer(process.env.ICLOUD_PASSWORD, 'base64').toString(), event.phone, function () {
-        console.log('Successfully found ', event.phone);
-    });
+    try {
+        find(process.env.ICLOUD_USERNAME, new Buffer(process.env.ICLOUD_PASSWORD, 'base64').toString(), event.phone, function () {
+            console.log('Successfully found ', event.phone);
+            context.succeed();
+        });
+    } catch (e) {
+        console.log('error')
+        console.log(e);
+        context.done();
+    }
+
+
 };
